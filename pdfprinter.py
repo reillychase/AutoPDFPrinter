d4a13d0ae671871ec3dc0a72b86fb6c56ca7b97d
reillychase <rchase@rchase.com>
2016-03-08 13:29:08 -0600

    pdfprinter.py

---
+++ pdfprinter.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# Import Web Browser
+import mechanize
+
+# Import HTML Parser
+from bs4 import BeautifulSoup
+
+# For Downloading PDFs
+import urllib2
+
+# For Printing PDFs
+import _winreg as winreg
+import time, os, subprocess
+
+# Dynamically get path to AcroRD32.exe
+AcroRD32Path = winreg.QueryValue(winreg.HKEY_CLASSES_ROOT,'Software\\Adobe\\Acrobat\Exe')
+acroread = AcroRD32Path
+
+# Download PDF Function
+def download_file(download_url, pdf_name):
+    response = urllib2.urlopen(download_url)
+    file = open(pdf_name, 'wb')
+    file.write(response.read())
+    file.close()
+    print("Downloaded PDF: " + pdf_name)
+
+############################
+# Setup Mechanize Defaults #
+############################
+br = mechanize.Browser()
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+http_link = raw_input("Enter link to site: ")
+
+attempts = 0
+while attempts < 5:
+    try:
+        # User-Agent
+        br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko')]
+        r = br.open(http_link)
+        page = r.read()
+        soup = BeautifulSoup(page)
+        anchor_links = soup.find_all("a", href=True)
+        break
+
+    except:
+        print "ERROR: Connection failed to " + http_link
+        attempts = attempts + 1
+
+for link in anchor_links:
+    if '.pdf' in link.text:
+        pdf_link = link.get('href')
+        pdf_name = link.text
+
+        # Download the PDF
+        download_file(pdf_link, pdf_name)
+
+        # The last set of double quotes leaves the printer blank, using the default printer
+        cmd= '{0} /N /T "{1}" ""'.format(acroread, pdf_name)
+
+        # See what the command line will look like before execution
+        print(cmd)
+
+        # Open command line in a different process
+        proc = subprocess.Popen(cmd)
+
+        # Wait for PDF to be sent to printer
+        time.sleep(1)
+
+        # Delete the PDF from file system after printing.
+        os.remove(pdf_name)
+
+
+# After all PDFs have printed, wait 1 second then kill Adobe Reader
+time.sleep(1)
+
+# Kill AcroRD32.exe from Task Manager
+os.system("TASKKILL /F /IM AcroRD32.exe")
